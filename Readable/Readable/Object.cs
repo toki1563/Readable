5092ba4c6e620c840077fe7d1a466185a7ec6899
sanda <sanda.yuto0603@gmail.com>
2024-03-05 22:11:49 +0900

    5コース目
    5の0：50までした

---
+++ Readable/Readable/Object.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Readable
+{
+    // sealed(継承できないように)
+    public sealed class Product
+    {
+        public Product(int productId, string productName, int price, int stock)
+        {
+            ProductId = productId;
+            ProductName = productName;
+            Price = price;
+            Stock = stock;
+        }
+
+        public int ProductId { get; }
+        public string ProductName { get; }
+        public int Price { get; }
+        public int Stock { get; }
+    }
+
+
+    public sealed class ProductDto
+    {
+        public ProductDto(Product product)
+        {
+            ProductId = product.ProductId.ToString();
+            ProductName = product.ProductName;
+            Price = product.Price.ToString();
+            Stock = product.Stock.ToString();
+        }
+
+        public string ProductId { get; }
+        public string ProductName { get; }
+        public string Price { get; }
+        public string Stock { get; }
+    }
+
+
+    public static class ProductSqlServer
+    {
+        public static IEnumerable<Product> GetProducts()
+        {
+            var result = new List<Product>();
+            result.Add(new Product(10, "p10", 100, 2));
+            result.Add(new Product(20, "p20", 200, 4));
+            result.Add(new Product(30, "p30", 300, 8));
+            return result;
+        }
+    }
+}
---
+++ Readable/Readable/ReadableCode.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Readable
+{
+    public class ReadableCode
+    {
+        // BAD    : 良くない
+        // NOTBAD : 別に悪くはない
+        // GOOD   : いい書き方、推奨
+        private void 右に長いコードを書かない()
+        {
+            var dtos = new List<ProductDto>();
+            // BAD:右に長いコードを書かない(となりぐらいまでにする)
+            ProductSqlServer.GetProducts().Where(x => x.Price > 100).ToList().ForEach(x => dtos.Add(new ProductDto(x)));
+
+            // 改行しても右に長いコードには変わりない
+            ProductSqlServer.GetProducts()
+                .Where(x => x.Price > 100)
+                .ToList().ForEach(x => dtos.Add(new ProductDto(x)));
+
+            // ListのForEachをするためにToListするのは冗長
+            var products1 = ProductSqlServer.GetProducts().Where(x => x.Price > 100).ToList();
+            products1.ForEach(x => dtos.Add(new ProductDto(x)));
+
+            // GOOD:隣のとなりまでしか訪ねない
+            var products2 = ProductSqlServer.GetProducts().Where(x => x.Price > 100);
+            foreach (var product in products2)
+            {
+                dtos.Add(new ProductDto(product));
+            }
+
+            // 右に長くなるという事とクラスをうまく分割できていない
+        }
+
+        // 判断(if文)
+        // IFとELSEがある場合はIFはTrueの時に入る
+        private void IFとELSEがある場合はIFはTrueの時に入る()
+        {
+
+        }
+    }
+}
